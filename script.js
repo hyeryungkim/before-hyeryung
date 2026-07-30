e02f0ea21248ce65ebecf4796ad51359679a4413
hyeryungkim <hyeryungkim.kr@gmail.com>
2026-07-31 00:33:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,122 +5,122 @@ const galleryData = [
     {
         src: 'images/001.jpg',
         title: 'ROOM by ROOM _ B13-1',
-        desc: 'H15 Φ11cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Ø10 × H11.5 cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/002.jpg',
         title: 'ROOM by ROOM _ B13-2',
-        desc: 'H15 Φ11cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Ø10 × H11 cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/003.jpg',
         title: 'ROOM by ROOM _ B13-3',
-        desc: 'H15 Φ11cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Ø10 × H11.3 cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/004.jpg',
         title: 'ROOM by ROOM _ G11',
-        desc: 'Each: H10 Φ12cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Each: Ø12 × H10 cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/005.jpg',
         title: 'ROOM by ROOM _ W3',
-        desc: 'H7 Φ10cm\nPorcelain, wheel throwing, reduction firing 1250℃'
+        desc: 'Ø10 × H7 cm\nPorcelain, wheel throwing, reduction firing 1250℃'
     },
     {
         src: 'images/006.jpg',
         title: 'ROOM by ROOM _ W1,2',
-        desc: 'Each: H7 Φ10cm\nPorcelain, wheel throwing, reduction firing 1250℃'
+        desc: 'Each: Ø10 × H7 cm\nPorcelain, wheel throwing, reduction firing 1250℃'
     },
     {
         src: 'images/007.jpg',
         title: 'Structural Bonding _ pipeline',
-        desc: 'H9.5 Φ5.5cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Ø5.7 × H9.5 cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/008.jpg',
         title: 'Structural Bonding _ pipeline',
-        desc: ' - cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Variable dimensions\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/009.jpg',
         title: 'In Sunlight or in Shadow _ B17',
-        desc: 'Each: H8 x 20 x 20cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Variable dimensions\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/010.jpg',
-        title: 'AZIT _ Tranquil shadow 3',
-        desc: '31.8 x 31.8 cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        title: 'In Sunlight or in Shadow _ G3',
+        desc: 'Ø10.2 × H12.8 cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/011.jpg',
         title: 'ROOM by ROOM _ G23',
-        desc: 'Each: H12 Φ15cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Each: Ø14 × H7 cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/012.jpg',
         title: 'Tranquil Container _ B0',
-        desc: 'H9 Φ10cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Ø9 × H10 cm, Ø7.4 × H5 cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/013.jpg',
         title: 'True Color',
-        desc: 'Each: H10 Φ12cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Variable dimensions\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/014.jpg',
         title: 'ROOM by ROOM _ B5',
-        desc: 'Each: H4.2 Φ9.5cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Variable dimensions\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/015.jpg',
         title: 'ROOM by ROOM _ LL1',
-        desc: 'Each: H13 Φ13cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Variable dimensions\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/016.jpg',
         title: 'ROOM by ROOM _ V8,9',
-        desc: 'H4 Φ8cm, 8 x 17.2 x 8cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Ø8 × H4.8 cm, H4.8 × W17.2 × D8 cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/017.jpg',
         title: 'ROOM by ROOM _ KY2',
-        desc: ' H8 Φ13cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: ' Ø13 × H8 cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/018.jpg',
         title: 'Texure Container (black, white, blue)',
-        desc: 'Each: H10 Φ10cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Each: Ø9.7 × H9.6 cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/019.jpg',
         title: 'Gold dot Container',
-        desc: 'H5 Φ10cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Variable dimensions\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/020.jpg',
         title: 'Traquil container  ( handle series )',
-        desc: 'H20 Φ10cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Ø12 × H11.7 cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/021.jpg',
         title: 'Texure Container',
-        desc: 'H9 Φ10m, H3 Φ10cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Ø8 × H10.3 cm, Ø8 × H6.4 cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/022.jpg',
         title: 'Rhythm Bowl (yellow)',
-        desc: '45 x 45 x 20cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Variable dimensions\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/023.jpg',
         title: 'Rhythm Bowl (Black, White)',
-        desc: 'Each: H13 Φ15cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Variable dimensions\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/024.jpg',
         title: 'ROOM by ROOM _ KY0',
-        desc: 'H8 Φ13cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Ø8 × H4.6 cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
     },
     {
         src: 'images/025.jpg',
@@ -130,7 +130,7 @@ const galleryData = [
     {
         src: 'images/026.jpg',
         title: 'TRANQUIL CONTAINER',
-        desc: 'Each: H16 Φ15cm\nPorcelain, wheel throwing, oxidation firing 1250℃'
+        desc: 'Variable dimensions\nPorcelain, wheel throwing, oxidation firing 1250℃'
     }
 ];
 
